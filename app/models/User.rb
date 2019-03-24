602e937e4febf4d6bf6548b2ff8ce4024e09b9af
github username <github email address>
2019-03-23 18:04:04 -0700

    Add validations

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, uniqueness: {case_sensitive: false}, presence: true
+  validates :username, uniqueness: {case_sensitive: false}, presence: true
+  validates :firstname, :lastname, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
   #has username, firstname, lastname, email, password_digest, image
 
   # has_many :favorites
