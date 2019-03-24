cdacf845aa1524a4de92565a07d0737c1412cf8a
github username <github email address>
2019-03-24 15:35:29 -0700

    Add className

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchHikes from './Hikes/SearchHikes';
 
 function Home() {
-  return <div id="home"> <SearchHikes /></div>
+  return <div id="home" className='search-form'> <SearchHikes /></div>
 };
 
 export default Home;
