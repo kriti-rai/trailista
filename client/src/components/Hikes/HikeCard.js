1c09e666b757dec4a9d560afb7e391dff0e82016
github username <github email address>
2019-03-24 16:38:43 -0700

    Add className

--- client/src/components/Hikes/HikeCard.js
+++ client/src/components/Hikes/HikeCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 function HikeCard(props) {
   const hike = props.hike
   return (
-    <li>
+    <div className="hike-card">
       <h5>{ hike.title }</h5>
       <p>{ hike.difficulty }</p>
       <p>{ hike.ratings }</p>
-    </li>
+    </div>
   )
 }
 
