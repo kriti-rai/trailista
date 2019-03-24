b5c2e8ab8c09768395afc2afc7b0acc365a7c9eb
github username <github email address>
2019-03-24 15:35:52 -0700

    Remove className

--- client/src/components/Hikes/SearchHikes.js
+++ client/src/components/Hikes/SearchHikes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SearchHikes() {
   return (
-    <form className='search-form'>
+    <form >
       <input className='form-control-lg' id='searchbox' type='text' placeholder="Enter a city..." />
     </form>
   )
