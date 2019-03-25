6031ee97eb619f1c7d6037a0ebbf4bf833a5a29e
github username <github email address>
2019-03-24 21:38:46 -0700

    Remove <Router> and routes

--- client/src/components/Hikes/HikesList.js
+++ client/src/components/Hikes/HikesList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import HikeCard from './HikeCard';
-
+import { Link } from 'react-router-dom'
 
 function HikesList(props) {
   return (
     <div>
-      { props.hikes.map((hike,id) => <HikeCard key={ id } hike={ hike }/>) }
+      { props.hikes.map((hike,id) => <Link to={`/hikes/${ id }`}><HikeCard key={ id } hike={ hike }/></Link>) }
     </div>
   )
 }
--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Register from '.././components/UserRegistration/SignUp';
-import Login from '.././components/UserRegistration/Login';
-import About from '.././components/About';
-import User from '.././components/User';
-import Home from '.././components/Home';
-import HikesContainer from './HikesContainer';
+import { NavLink } from "react-router-dom";
+import '.././styles/NavBar.css';
 
 
 function Navbar() {
   return (
     <div>
-      <Router>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Link to='/hikes' className="nav-link">Explore</Link>
-                <Link to='/about' className="nav-link">About</Link>
-                <h3><Link to='/' className="nav-link">MyApp</Link></h3>
-                <Link to='/login' className="nav-link">Log in</Link>
-                <Link to='/signup' className="nav-link">Sign up</Link>
-            </div>
-          </nav>
-          <Switch>
-            <Route exact path="/" exact component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/user" component={User} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Register} />
-            <Route exact path="/hikes" component={HikesContainer} />
-            <Route render={() => <h2 class="400-error">404 Error - Page not found</h2>} />
-          </Switch>
-      </Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <NavLink to='/hikes' className="nav-link">Explore</NavLink>
+            <NavLink to='/about' className="nav-link">About</NavLink>
+            <h3><NavLink to='/' className="nav-link">MyApp</NavLink></h3>
+            <NavLink to='/login' className="nav-link">Log in</NavLink>
+            <NavLink to='/signup' className="nav-link">Sign up</NavLink>
+        </div>
+      </nav>
     </div>
   );
 };
