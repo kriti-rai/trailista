ba55b85f329f002ed3c2808a1465015e533c6df7
github username <github email address>
2019-03-25 00:15:43 -0700

    Render Hike details

--- client/src/components/Hikes/Hike.js
+++ client/src/components/Hikes/Hike.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-function Hike(props) {
-  const hike = props.hike
+function Hike({ match, hikes }) {
   return (
     <div>
-      <h3>{ hike.title }</h3>
-      <p>{ hike.difficulty }</p>
-      <p>{ hike.ratings }</p>
-      <p><img src={ hike.image} /></p>
-      Length:{ hike.length }
-      Ascent:{ hike.ascent }
-      Descent:{ hike.descent }
+      <h3>{ hikes[match.params.hikeId].title }</h3>
+      <p>Difficulty: { hikes[match.params.hikeId].difficulty }</p>
+      <p>Rating: { hikes[match.params.hikeId].ratings }</p>
+      <p>Length:{ hikes[match.params.hikeId].length }</p>
+      <p>Ascent:{ hikes[match.params.hikeId].ascent }</p>
+      <p>Descent:{ hikes[match.params.hikeId].descent }</p>
+      <p><img src={ hikes[match.params.hikeId].image} /></p>
+      <p>{ hikes[match.params.hikeId].overview }</p>
+
     </div>
   )
 }
