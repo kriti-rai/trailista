3312160f88a177437289398c9cd1aea749e6446c
github username <github email address>
2019-03-24 21:48:53 -0700

    Define componentDidMount(); add state; pass props to HikesContaienr

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import './styles/HikeCard.css';
-import './styles/NavBar.css';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './containers/Navbar';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -14,7 +12,20 @@ import Home from './components/Home';
 import HikesContainer from './containers/HikesContainer';
 import Hike from './components/Hikes/Hike';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 class App extends Component {
+  state = {
+    hikes: []
+  }
+
+
+  componentDidMount() {
+    fetch(`${API_URL}/hikes`)
+      .then(resp => resp.json())
+      .then(hikes => this.setState({ hikes }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,7 +37,7 @@ class App extends Component {
               <Route exact path="/user" component={User} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Register} />
-              <Route path="/hikes" component={HikesContainer} />
+              <Route path="/hikes" render={ routerProps => <HikesContainer {...routerProps} hikes={ this.state.hikes }/> } />
               <Route render={() => <h2 class="400-error">404 Error - Page not found</h2>} />
             </Switch>
         </Router>
