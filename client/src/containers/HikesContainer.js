1a93f95e0a75c7873b64e111bfca279b4f534989
github username <github email address>
2019-03-25 00:20:13 -0700

    Add nested route to render Hike

--- client/src/containers/HikesContainer.js
+++ client/src/containers/HikesContainer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SearchHikes from '.././components/Hikes/SearchHikes';
 import HikesList from '.././components/Hikes/HikesList';
+import { Route } from 'react-router-dom'
+import Hike from '.././components/Hikes/Hike';
+
 import { connect } from 'react-redux';
 
 const HikesContainer = (props) => {
     return (
       <div>
         <SearchHikes />
-        <HikesList hikes={ props.hikes } url={ props.match.url}/>
+        <HikesList hikes={ props.hikes } />
+        <Route path={`${props.match.url}/:hikeId`} render={(routerProps) => <Hike {...routerProps} hikes={ props.hikes } /> }/>
       </div>
     )
 };
