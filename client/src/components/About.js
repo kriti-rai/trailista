e9e666f67e5eeb3ca21edd99920a00097fa8ff8a
github username <github email address>
2019-03-26 00:46:19 -0700

    Add class

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="container">
-      <div className="d-flex justify-content-center h-100">
-        <div className='card'>
-          <h3 className="card-header">About</h3>
-          <p><font color="white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </font>
-          </p>
+    <div class="about">
+      <div className="container">
+        <div className="d-flex justify-content-center h-100">
+          <div className='card'>
+            <h3 className="card-header">About</h3>
+            <p><font color="white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </font>
+            </p>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   )
 };
