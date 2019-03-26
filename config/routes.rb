219550b5c7933f5fb444d0e47b297c764bc56be5
github username <github email address>
2019-03-25 18:00:34 -0700

    Add route to user_token#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :hikes, only: [:index]
+    resources :users
+    post 'user_token' => 'user_token#create'
   end
 
-  namespace :api do
-    resources :users
-  end
 end
