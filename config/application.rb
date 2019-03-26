01233cba274dfb0275082703d6a75d520fe76a46
github username <github email address>
2019-03-26 12:39:58 -0700

    Change module name

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ConcertSpotter
+module Trailista
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +30,6 @@ module ConcertSpotter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # config.autoload_paths << Rails.root.join('lib')
   end
 end
