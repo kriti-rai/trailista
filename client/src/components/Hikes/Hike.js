7956f32c6d742bef4845848a324490fbf75580cd
github username <github email address>
2019-03-26 01:32:28 -0700

    Add className

--- client/src/components/Hikes/Hike.js
+++ client/src/components/Hikes/Hike.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hike({ match, hikes }) {
   return (
-    <div>
+    <div className="hikes-container">
       <h3>{ hikes[match.params.hikeId].title }</h3>
       <p>Difficulty: { hikes[match.params.hikeId].difficulty }</p>
       <p>Rating: { hikes[match.params.hikeId].ratings }</p>
@@ -10,7 +10,7 @@ function Hike({ match, hikes }) {
       <p>Ascent:{ hikes[match.params.hikeId].ascent }</p>
       <p>Descent:{ hikes[match.params.hikeId].descent }</p>
       <p><img src={ hikes[match.params.hikeId].image} /></p>
-      <p>{ hikes[match.params.hikeId].overview }</p>
+      <small className="hike-overview">{ hikes[match.params.hikeId].overview }</small>
 
     </div>
   )
