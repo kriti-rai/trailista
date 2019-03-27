b5f4220b47d2645078751dee36c406c8b84147a5
github username <github email address>
2019-03-27 01:51:15 -0700

    Import axios update loginUser()

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import axios from 'axios';
 
-function authenticate(id) {
-  type:'AUTHENTICATE_USER',
-  id
+const baseUrl = "http://localhost:3001/api";
+
+export function loginUser(user) {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_USER_INFO" })
+    return axios.post(`${baseUrl}/sessions`, user)
+      .then(response => {
+        console.log(response)
+        localStorage .setItem("logged_in", 'true')
+        localStorage .setItem("jwt", response.data.jwt)
+        dispatch({
+          type: 'AUTHENTICATE_USER',
+          payload: response.data
+        })
+      })
+      // .catch(error => {
+      //   dispatch({ type: 'SHOW_ERROR', message: error.response.data.error})
+      // })
+  }
 }
-
-export default authenticate;
