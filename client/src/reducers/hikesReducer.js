98046d72f08b9f26dc2411262eae868adbfc930c
github username <github email address>
2019-03-27 01:43:22 -0700

    Rename

---
+++ client/src/reducers/hikesReducer.js
@@ -0,0 +1,12 @@
+function hikesReducer(state=[], action) {
+  switch (action.type) {
+    case 'LOADING_HIKES':
+      return state;
+    case 'FETCH_HIKES':
+      return action.hikes.data;
+    default:
+      return state;
+  }
+}
+
+export default hikesReducer;
