721535617f6c24b1fd7606faf1f3fd5f8fa91150
github username <github email address>
2019-03-27 01:37:11 -0700

    Correct class name; def #create and private methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,9 @@
-class Api::UsersController < ApplicationController
+class Api::SessionsController < ApplicationController
   skip_before_action :authenticate
-  
-  def create
-    user = User.find_by(username: user_params[:username])
 
-    if user.authenticate(auth_params[:password])
+  def create
+    user = User.find_by(username: params["username"])
+    if user && user.authenticate(params["password"])
       jwt = Auth.issue({user: user.id})
       render json: {jwt: jwt}
     end
