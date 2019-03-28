097746edf869700f33a555aeca25a81414f1ebd2
github username <github email address>
2019-03-27 18:31:14 -0700

    Import Fragment; add conditions to show profile and logout tabs

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from "react-router-dom";
-import '.././styles/NavBar.css';
 
 
 function Navbar() {
+  var userActions;
+
+  if (!!window.localStorage.jwt) {
+    var userActions = (
+      <>
+        <NavLink to='/users/:user_id' className="nav-link">My Profile</NavLink>
+        <NavLink to='/' className="nav-link">Logout</NavLink>
+      </>
+    )
+  } else {
+    var userActions = (
+      <>
+        <NavLink to='/login' className="nav-link">Log in</NavLink>
+        <NavLink to='/signup' className="nav-link">Sign up</NavLink>
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
             <NavLink to='/hikes' className="nav-link">Explore</NavLink>
             <NavLink to='/about' className="nav-link">About</NavLink>
-            <h3><NavLink to='/' className="nav-link">MyApp</NavLink></h3>
-            <NavLink to='/login' className="nav-link">Log in</NavLink>
-            <NavLink to='/signup' className="nav-link">Sign up</NavLink>
+            <h3><em><NavLink to='/' className="nav-link">Trailista</NavLink></em></h3>
+            { userActions }
         </div>
       </nav>
-    </div>
+    </>
   );
 };
 
