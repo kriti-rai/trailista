98ab863793c0bd889c5976d202ae54f4328c5486
github username <github email address>
2019-03-27 17:10:30 -0700

    Fix user_params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,7 +20,7 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :firstname, :lastname)
+    params.require(:user).permit(:username, :email, :password_digest, :firstname, :lastname)
   end
 
 end
