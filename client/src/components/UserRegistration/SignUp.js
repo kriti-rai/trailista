d3d320a9a005757cca2bb7945bd1be1425d2dfea
github username <github email address>
2019-03-27 17:11:42 -0700

    Import connect and registerUser(); rename attributes in state and form data

--- client/src/components/UserRegistration/SignUp.js
+++ client/src/components/UserRegistration/SignUp.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { registerUser } from '../.././actions/userActions';
 
 class Register extends Component {
   constructor() {
     super();
     this.state = {
       username: "",
-      firstName: "",
-      lastName: "",
+      firstname: "",
+      lastname: "",
       email: "",
-      password: "",
+      password_digest: "",
     }
   }
 
@@ -20,42 +22,45 @@ class Register extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('you hit submit') //create user
+    const data =  this.state;
+    this.props.registerUser(data, () => this.props.history.push('/'));
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="d-flex justify-content-center h-100">
-          <div className="card">
-            <h3 className="card-header">Register</h3>
-            <div className="card-body">
-              <form className='login-form' onSubmit={ this.handleSubmit }>
-                <div className="input-group form-group">
-                    <input type="text" name="username" value={ this.state.username } className="form-control" placeholder="Username"  onChange={ this.handleChange } />
-                </div>
-                <div className="input-group form-group">
-                    <input type="text" name="firstName" value={ this.state.firstName } className="form-control" placeholder="First Name" onChange={ this.handleChange } />
-                </div>
-                <div className="input-group form-group">
-                    <input type="text" name="lastName" value={ this.state.lastName } className="form-control" placeholder="Last Name" onChange={ this.handleChange } />
-                </div>
-                <div className="input-group form-group">
-                    <input type="text" name="email" value={ this.state.email } className="form-control" placeholder="Email Address" onChange={ this.handleChange } />
-                </div>
+      <div className="signup">
+        <div className="container">
+          <div className="d-flex justify-content-center h-100">
+            <div className="card">
+              <h3 className="card-header">Register</h3>
+              <div className="card-body">
+                <form className='login-form' onSubmit={ this.handleSubmit }>
                   <div className="input-group form-group">
-                    <input type="password" name="password" value={ this.state.password } className="form-control" placeholder="Password" onChange={ this.handleChange } />
+                      <input type="text" name="username" value={ this.state.username } className="form-control" placeholder="Username"  onChange={ this.handleChange } />
                   </div>
-                  <div className="form-group">
-                    <input className="button login_btn" type="submit" value="Sign Up"/>
+                  <div className="input-group form-group">
+                      <input type="text" name="firstname" value={ this.state.firstname } className="form-control" placeholder="First Name" onChange={ this.handleChange } />
                   </div>
-              </form>
+                  <div className="input-group form-group">
+                      <input type="text" name="lastname" value={ this.state.lastname } className="form-control" placeholder="Last Name" onChange={ this.handleChange } />
+                  </div>
+                  <div className="input-group form-group">
+                      <input type="text" name="email" value={ this.state.email } className="form-control" placeholder="Email Address" onChange={ this.handleChange } />
+                  </div>
+                    <div className="input-group form-group">
+                      <input type="password" name="password_digest" value={ this.state.password_digest } className="form-control" placeholder="Password" onChange={ this.handleChange } />
+                    </div>
+                    <div className="form-group">
+                      <input className="button login_btn" type="submit" value="Sign Up"/>
+                    </div>
+                </form>
+                </div>
               </div>
             </div>
-          </div>
+        </div>
       </div>
     )
   }
 };
 
-export default Register;
+export default connect(null, { registerUser })(Register);
