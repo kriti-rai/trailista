9b30d22361c79870186b1244bf65ad5da136ca63
github username <github email address>
2019-03-29 12:20:59 -0700

    Update SHOW_USER case to add user to the state

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -8,6 +8,16 @@ function userReducer(state={logged_in: "false", token: ""}, action) {
       } else {
         return {logged_in: "true", token: action.payload.jwt};
       }
+    case 'SHOW_USER':
+      debugger
+      let user = {
+        username: action.payload.username,
+        firstName: action.payload.firstname,
+        lastName: action.payload.lastname,
+        email: action.payload.email,
+        image: action.payload.image
+      }
+      return { ...state, user: user };
     case 'LOGOUT':
       return {logged_in: "false", token: ""};
     default:
