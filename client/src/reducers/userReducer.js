cfe42d496fa981ab140f8446204fa79c3d3d9234
github username <github email address>
2019-03-28 20:29:04 -0700

    Update AUTHENTICATE_USER case

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,12 +1,15 @@
-function userReducer(state=[{logged_in: "false", token: ""}], action) {
+function userReducer(state={logged_in: "false", token: ""}, action) {
   switch (action.type) {
     case 'LOADING_USER_INFO':
       return state;
     case 'AUTHENTICATE_USER':
-      // debugger
-      return [...state, {logged_in: "true", token: action.payload.jwt}];
+      if (!action.payload.hasOwnProperty('jwt')) {
+        return state;
+      } else {
+        return {logged_in: "true", token: action.payload.jwt};
+      }
     case 'LOGOUT':
-      return [...state, {logged_in: "false", token: action.payload}];
+      return {logged_in: "false", token: ""};
     default:
       return state;
   }
