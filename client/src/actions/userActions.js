40ea57b2114afba765f7959627d9a7676d871fa5
github username <github email address>
2019-03-28 20:28:33 -0700

    Add/update logout(); remove authenticate()

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const baseUrl = "http://localhost:3001/api";
 
-export function loginUser(user, callback) {
+export function login(user, callback) {
   return (dispatch) => {
     dispatch({ type: "LOADING_USER_INFO" })
     return axios.post(`${baseUrl}/sessions`, user)
       .then(response => {
         console.log(response)
-        localStorage .setItem("logged_in", 'true')
-        localStorage .setItem("jwt", response.data.jwt)
+        localStorage.setItem("logged_in", 'true')
+        localStorage.setItem("jwt", response.data.jwt)
         dispatch({
           type: 'AUTHENTICATE_USER',
           payload: response.data
@@ -20,4 +20,55 @@ export function loginUser(user, callback) {
       //   dispatch({ type: 'SHOW_ERROR', message: error.response.data.error})
       // })
   }
+}
+
+export function signup(user, callback) {
+  // debugger
+  return (dispatch) => {
+    dispatch({ type: "LOADING_USER_INFO" })
+    return axios.post(`${baseUrl}/users`, user)
+      .then(response => {
+        console.log(response)
+        localStorage.setItem("logged_in", 'true')
+        localStorage.setItem("jwt", response.data.jwt)
+        dispatch({
+          type: 'AUTHENTICATE_USER',
+          payload: response.data
+        })
+        callback()
+      })
+      // .catch(error => {
+      //   dispatch({ type: 'SHOW_ERROR', message: error.response.data.error})
+      // })
+  }
+}
+
+export function logout() {
+  if (localStorage['jwt']) {
+    localStorage.removeItem('jwt')
+    localStorage.removeItem('logged_in')
+  }
+
+  return (dispatch) => {
+    return axios.post(`${baseUrl}/logout`)
+      .then(response => {
+        dispatch({
+          type: 'LOGOUT',
+          payload: ""
+        })
+      })
+    }
+}
+
+export function fetchUser(username) {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_USER_INFO" })
+    return axios.get(`${baseUrl}/users/${username}`)
+      .then(response => {
+        dispatch({
+          type: 'SHOW_USER',
+          payload: response.data
+        })
+      })
+    }
 }
