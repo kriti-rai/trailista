cc12ccdb91c78b0664b821eb7e5d641248e49d60
github username <github email address>
2019-03-28 20:26:23 -0700

    Add showTabs and render them depending on if logged_in is true or false

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -2,37 +2,39 @@ import React, { Fragment } from 'react';
 import { NavLink } from "react-router-dom";
 
 
-function Navbar() {
-  var userActions;
+function Navbar(props) {
+  let logged_in = (props.logged_in === "true") ? true : false;
 
-  if (!!window.localStorage.jwt) {
-    var userActions = (
+  let showTabs;
+
+  if (logged_in) {
+    showTabs = (
       <>
-        <NavLink to='/users/:user_id' className="nav-link">My Profile</NavLink>
-        <NavLink to='/' className="nav-link">Logout</NavLink>
+        <NavLink to='/user' className="nav-link">My Profile</NavLink>
+        <NavLink to='/logout' className="nav-link">Logout</NavLink>
       </>
     )
   } else {
-    var userActions = (
+    showTabs = (
       <>
-        <NavLink to='/login' className="nav-link">Log in</NavLink>
-        <NavLink to='/signup' className="nav-link">Sign up</NavLink>
+        <NavLink to='/login' className="nav-link">Login</NavLink>
+        <NavLink to='/signup' className="nav-link">Signup</NavLink>
       </>
     )
   }
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <NavLink to='/hikes' className="nav-link">Explore</NavLink>
-            <NavLink to='/about' className="nav-link">About</NavLink>
-            <h3><em><NavLink to='/' className="nav-link">Trailista</NavLink></em></h3>
-            { userActions }
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+          <NavLink to='/hikes' className="nav-link">Explore</NavLink>
+          <NavLink to='/about' className="nav-link">About</NavLink>
+          <h3><em><NavLink to='/' className="nav-link">Trailista</NavLink></em></h3>
+          { showTabs }
         </div>
       </nav>
     </>
-  );
-};
+  )
+}
 
 export default Navbar;
