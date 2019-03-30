9244311a90162b62cd6985f892b33a870ba4233e
github username <github email address>
2019-03-30 01:53:51 -0700

    Set up FavoritesContainer

---
+++ client/src/containers/FavoritesContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesContainer = (props) => {
+    return (
+      <div className="favorites-container">
+        <small>(render my favorite hikes)</small>
+      </div>
+    )
+};
+
+export default FavoritesContainer;
