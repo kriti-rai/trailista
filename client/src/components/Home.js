5a69ed34b58ab0b719a714384891931d3077c6cc
github username <github email address>
2019-03-30 01:51:59 -0700

    Add welcome message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import SearchHikes from './Hikes/SearchHikes';
 
 function Home() {
-  return <div id="home" className='search-form'> <SearchHikes /></div>
+  return  (
+    <>
+      <div className="home-container">
+        <p className="welcome-msg">Welcome to Trailista</p>
+        <div className='search-form'> <SearchHikes /></div>
+      </div>
+    </>
+  )
 };
 
 export default Home;
