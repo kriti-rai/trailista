390a674caa3a452788518d095b30b2ac61553ffc
github username <github email address>
2019-03-30 01:52:39 -0700

    Add <hr> and <Favorites/>

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,23 +1,29 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { fetchCurrentUser } from '.././actions/userActions';
+import Favorites from '.././containers/FavoritesContainer';
 
 class User extends Component {
-  constructor() {
-    super();
-  }
 
   componentDidMount() {
-    this.props.fetchCurrentUser()
+    this.props.fetchCurrentUser(this.props.token)
   }
 
   render() {
-    // debugger
     return (
       <>
-      <h2>User Profile</h2>
+      <div className='profile-container'>
+        <img className="profile-pic" src='https://static1.squarespace.com/static/51c45c5ae4b05645eb831a3e/t/57e98e9ce58c621cdb06c5e2/1550728951497/' alt="profile-pic" />
+        <div className='user-data'>
+          <h3><em>{this.props.currentUser.username}</em></h3>
+          <h3>{ this.props.currentUser.firstName } { this.props.currentUser.lastName }</h3>
+          <hr></hr>
+          <h4>My Favorites &hearts;</h4>
+          <Favorites />
+        </div>
+      </div>
       </>
-  )}
-};
+    )
+  }
+}
 
-export default connect(null, { fetchCurrentUser })(User);
+export default connect(state => ({ currentUser: state.user.user }))(User);
