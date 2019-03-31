43f4dc799a741a86c8114bb2487e2470256bd999
github username <github email address>
2019-03-31 02:49:18 -0700

    Add has_many relations

--- app/models/Hike.rb
+++ app/models/Hike.rb
@@ -1,4 +1,6 @@
 class Hike < ApplicationRecord
   #has title, overview, image, length, ascent, descent, ratings, difficulty, comments
   has_many :comments
+  has_many :hikes_users
+  has_many :users, through: :hikes_users
 end
