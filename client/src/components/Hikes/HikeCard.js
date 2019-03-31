9aba7c9c4aab0bfb4a6ad017e82071d2bc84f5f9
github username <github email address>
2019-03-30 19:26:31 -0700

    Change order of image and hike info; add new class hike-info

--- client/src/components/Hikes/HikeCard.js
+++ client/src/components/Hikes/HikeCard.js
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 const HikeCard = ({ hike }) => {
   return (
     <div className="hike-card">
-      <h5>{  hike.title }</h5>
-      <p><small>Difficult: {  hike.difficulty }</small></p>
-      <p><small>Rating: {  hike.ratings}</small></p>
       <img className="hike-card-image" src= { hike.image} />
+      <div className="hike-info">
+        <h5>{  hike.title }</h5>
+        <p><small>Difficult: {  hike.difficulty }</small></p>
+        <p><small>Rating: {  hike.ratings}</small></p>
+      </div>
     </div>
   )
 }
