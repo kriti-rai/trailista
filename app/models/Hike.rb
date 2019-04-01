44e159a854409e66d11755d7d735f8ca0cb63d5d
github username <github email address>
2019-03-31 17:31:13 -0700

    Add has_many associaition to favorited_hikes and users

--- app/models/Hike.rb
+++ app/models/Hike.rb
@@ -1,6 +1,6 @@
 class Hike < ApplicationRecord
   #has title, overview, image, length, ascent, descent, ratings, difficulty, comments
   has_many :comments
-  has_many :hikes_users
-  has_many :users, through: :hikes_users
+  has_many :favorited_hikes
+  has_many :users, through: :favorited_hikes
 end
