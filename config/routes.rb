e3a4ecbedc84492ebf0b11f2acc699f88a4ea5be
github username <github email address>
2019-04-01 00:00:29 -0700

    Change get to post '/favorites'

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resources :sessions, only: [:create]
     get '/show' => 'users#show'
-    get '/favorites' => 'users#favorites'
+    post '/favorites' => 'users#favorites'
     post '/logout' => 'sessions#logout'
   end
 
