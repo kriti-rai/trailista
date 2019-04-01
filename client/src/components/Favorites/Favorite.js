43e172f2cfe21a8d5c204be9e2f814555a7f9cc7
github username <github email address>
2019-04-01 12:48:33 -0700

    Render fav hike title with delete button

--- client/src/components/Favorites/Favorite.js
+++ client/src/components/Favorites/Favorite.js
@@ -1,7 +1,11 @@
-function Favorite() {
-  <li>
-    <h3>Favorite<h3>
-  </li>
+import React from 'react';
+
+function Favorite(props) {
+  return (
+    <p>
+      { props.favHike.title } <button className="del-btn"><i id="del-btn" class="fas fa-minus-circle"> </i></button>
+    </p>
+  )
 }
 
 export default Favorite;
