9428c072741bb969034bb3d9c88256cebe3ce17d
github username <github email address>
2019-03-31 23:43:25 -0700

    Update component to render Favorite

--- client/src/components/Favorites/FavoritesList.js
+++ client/src/components/Favorites/FavoritesList.js
@@ -1,7 +1,21 @@
+import React, { Fragment } from 'react';
 import Favorite from './Favorite';
 
-function FavoritesList() {
-  //iterate over array of hikes and render <Hike />
+function FavoritesList(props) {
+
+  let renderList;
+
+  if (props.favHikes.length > 0) {
+    renderList = props.favHikes.map((favHike, i) => <Favorite key={ i } favHike={ favHike } />)
+  } else {
+    renderList = <small>Currently, there is no item in here.</small>
+  }
+
+  return (
+    <>
+      { renderList }
+    </>
+  )
 }
 
 export default FavoritesList;
