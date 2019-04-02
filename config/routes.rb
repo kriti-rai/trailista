6bc10b3be865dfef27a8b35079151bf18d03d9a1
github username <github email address>
2019-04-01 19:31:25 -0700

    Remove '/favorites'

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resources :sessions, only: [:create]
     get '/show' => 'users#show'
-    get '/favorites' => 'users#favorites'
     post '/add_favorite' => 'user#remove_favorite'
     post '/remmove_favorite' => 'user#remove_favorite'
     post '/clear_favorites' => 'user#clear_favorites'
