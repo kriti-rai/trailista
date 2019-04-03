7a888a1516b8ca47f2dff8ba959a06337c8b4e16
github username <github email address>
2019-04-03 14:53:45 -0700

    Switch out attribute names to match the api response; define const difficulty

--- client/src/components/Hikes/HikeCard.js
+++ client/src/components/Hikes/HikeCard.js
@@ -14,14 +14,33 @@ class HikeCard extends Component {
 
     const hike = this.props.hike;
 
+
+    const difficulty = () => {
+      let level;
+      if (hike.difficulty === "blue") {
+        level = "Intermediate";
+      } else if (hike.difficulty === "greenBlue") {
+        level = "Easy/Intermediate";
+      } else if (hike.difficulty === "blueBlack") {
+        level = "Intermediate/Hard";
+      } else if (hike.difficulty === "black") {
+        level = "Hard"
+      } else if (hike.difficulty === "green") {
+        level = "Easy"
+      } else {
+        level = "N/A"
+      }
+      return level;
+    }
+
     return (
       <div className="hike-card">
         <button className="fav-btn" onClick={ handleClick } ><i className="fas fa-heart"></i></button>
-        <img className="hike-card-image" src= { hike.image} />
+        <img className="hike-card-image" src= { hike.imgMedium} />
         <div className="hike-info">
-          <h5>{  hike.title }</h5>
-          <small>Difficult: { hike.difficulty }</small>
-          <p><small>Rating: { hike.ratings }</small></p>
+          <h5>{  hike.name }</h5>
+          <small>Difficulty: { difficulty() }</small>
+          <p><small>Rating: { hike.stars }</small></p>
         </div>
       </div>
     )
