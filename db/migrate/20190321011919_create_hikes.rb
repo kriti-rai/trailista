524db1e0bbd5b6999e0ddd4e4d943dcfae417fe8
github username <github email address>
2019-04-03 16:35:45 -0700

    Add/update columns

--- db/migrate/20190321011919_create_hikes.rb
+++ db/migrate/20190321011919_create_hikes.rb
@@ -1,14 +1,29 @@
 class CreateHikes < ActiveRecord::Migration[5.2]
   def change
     create_table :hikes do |t|
-      t.string :title
-      t.text :overview
-      t.string :image
-      t.float :length
-      t.string :ascent
-      t.string :descent
-      t.float :ratings
-      t.string :difficulty
+      t.string "name", null: false
+      t.string "type"
+      t.string "summary"
+      t.string "difficulty"
+      t.float "stars"
+      t.integer "starVotes"
+      t.string "location"
+      t.string "imgSqSmall"
+      t.string "imgSmall"
+      t.string "imgSmallMed"
+      t.string "imgMedium"
+      t.float "length"
+      t.integer "ascent"
+      t.integer "descent"
+      t.integer "high"
+      t.integer "low"
+      t.float "longitude"
+      t.float "latitude"
+      t.string "conditionStatus"
+      t.string "conditionDetails"
+      t.datetime "conditionDate"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
     end
   end
 end
