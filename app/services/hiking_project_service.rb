69e8ccaee05b7841dd3cee1868ae8790d21342ab
github username <github email address>
2019-04-03 13:56:19 -0700

    Create folder for web service objects

---
+++ app/services/hiking_project_service.rb
@@ -0,0 +1,16 @@
+class HikingProjectService
+
+  def get_hikes
+    resp = Faraday.get 'https://www.hikingproject.com/data/get-trails?' do |req|
+      req.params['lat'] = 40.0274
+      req.params['lon'] = -105.2519
+      req.params['maxDistance'] = 10
+      req.params['key'] = '200433706-b893b66d00841f18007905dd3d568c32'
+    end
+
+    JSON.parse(resp.body)
+
+  end
+
+
+end
