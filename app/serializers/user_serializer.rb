b7b3f73ee7056a6a0b54d8d9444c9f2f3ab2ab9d
github username <github email address>
2019-04-03 17:56:45 -0700

    Add location and created_at

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :firstname, :lastname, :email, :image, :fav_hikes
+  attributes :id, :username, :firstname, :lastname, :email, :image, :created_at, :location, :fav_hikes
 end
