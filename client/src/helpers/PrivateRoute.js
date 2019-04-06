1e451752cebb201a324702587d74fff1be2f2136
github username <github email address>
2019-04-06 15:08:37 -0700

    Add privateRoute

---
+++ client/src/helpers/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+import { addAlertMessage } from '.././actions/AlertsActions';
+
+export function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !!localStorage.getItem('jwt') ? (
+          <Component {...props} {...rest}/>
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
