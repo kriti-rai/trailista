3ee122bed6ea3ab2fb5eb11c1a1baa3147b2268a
github username <github email address>
2019-04-07 00:38:20 -0700

    Define difficulty(); switch ratings with stars

--- client/src/components/Hikes/Hike.js
+++ client/src/components/Hikes/Hike.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
 function Hike({ match, hikes }) {
-  
+
   let id = +match.params.hikeId.replace((/hike_/), "")
   let hike = hikes.find(hike => hike.id === id)
+
+  const difficulty = () => {
+    let level;
+    if (hike.difficulty === "blue") {
+      level = "Intermediate";
+    } else if (hike.difficulty === "greenBlue") {
+      level = "Easy/Intermediate";
+    } else if (hike.difficulty === "blueBlack") {
+      level = "Intermediate/Hard";
+    } else if (hike.difficulty === "black") {
+      level = "Hard"
+    } else if (hike.difficulty === "green") {
+      level = "Easy"
+    } else {
+      level = "N/A"
+    }
+    return level;
+  }
 
   return (
     <div className="hikes-container">
       <h3>{ hike.name }</h3>
-      <p>Difficulty: { hike.difficulty }</p>
-      <p>Rating: { hike.ratings }</p>
+      <p>Difficulty: { difficulty() }</p>
+      <p>Rating: { hike.stars }</p>
       <p>Length:{ hike.length }</p>
       <p>Ascent:{ hike.ascent }</p>
       <p>Descent:{ hike.descent }</p>
