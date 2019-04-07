251d3c70e819d604c0520128109810177e14866e
github username <github email address>
2019-04-07 00:23:12 -0700

    Replace hardcoded key with ENV variable

--- app/services/hiking_project_service.rb
+++ app/services/hiking_project_service.rb
@@ -2,10 +2,10 @@ class HikingProjectService
 
   def get_hikes
     resp = Faraday.get 'https://www.hikingproject.com/data/get-trails?' do |req|
-      req.params['lat'] = 40.0274
+      req.params['lat'] = 40.0274   #these are default coordinates for Oakland, Ca
       req.params['lon'] = -105.2519
       req.params['maxDistance'] = 10
-      req.params['key'] = '200433706-b893b66d00841f18007905dd3d568c32'
+      req.params['key'] = ENV["REACT_APP_HIKING_PROJECT_API_KEY"]
     end
 
     JSON.parse(resp.body)["trails"]
