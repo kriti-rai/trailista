c90b8d79338d6aad655c807d4b0c05bc37800896
github username <github email address>
2019-04-07 01:22:09 -0700

    Edit comment

--- app/services/hiking_project_service.rb
+++ app/services/hiking_project_service.rb
@@ -2,7 +2,7 @@ class HikingProjectService
 
   def get_hikes
     resp = Faraday.get 'https://www.hikingproject.com/data/get-trails?' do |req|
-      req.params['lat'] = 40.0274   #these are default coordinates for Oakland, Ca
+      req.params['lat'] = 40.0274   #these are default coordinates for Boulder,CO
       req.params['lon'] = -105.2519
       req.params['maxDistance'] = 10
       req.params['key'] = ENV["REACT_APP_HIKING_PROJECT_API_KEY"]
