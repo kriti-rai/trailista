325c2b95249eaa7f85cf23b9fc0173c53b9e4f36
github username <github email address>
2019-04-08 12:17:15 -0700

    Remove fetchHike dispatch action

--- client/src/actions/hikeActions.js
+++ client/src/actions/hikeActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const HIKING_PROJECT_API_KEY = process.env.REACT_APP_HIKING_PROJECT_API_KEY;
+const API_URL = process.env.REACT_APP_API_URL;
 
 //Fetch hikes by coordinates
 
@@ -9,15 +10,5 @@ export function fetchHikes(latitude, longitude, maxDistance, maxResults) {
     dispatch({ type: 'LOADING_HIKES' });
     return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=${maxDistance}&maxResults=${maxResults}&key=${HIKING_PROJECT_API_KEY}`)
       .then(hikes => dispatch({ type: 'FETCH_HIKES', hikes }));
-  };
-}
-
-//Fetch a hike by ID
-
-export function fetchHike(id) {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_HIKE' });
-    return axios.get(`https://www.hikingproject.com/data/get-trails-by-id?ids=${id}&key&key=${HIKING_PROJECT_API_KEY}`)
-      .then(hike => dispatch({ type: 'FETCH_HIKE', hike }));
   };
 }
