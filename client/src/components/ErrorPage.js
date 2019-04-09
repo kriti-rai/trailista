d108f93664804f1e97476cf5567a13a43dee9743
github username <github email address>
2019-04-08 22:36:44 -0700

    Create ErrorPage

---
+++ client/src/components/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className="error-page-container">
+      <div className="display-02">
+        <span className="sad-face-icon"><i class="far fa-frown"></i></span>
+        <p className="lead">D'oh! can't find what you are looking for.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage;
