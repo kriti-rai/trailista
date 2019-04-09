258ca44f06ebf0b446a89d754694f3f37657ad2f
github username <github email address>
2019-04-08 18:44:31 -0700

    Update if condition

--- client/src/containers/Hikes/HikeContainer.js
+++ client/src/containers/Hikes/HikeContainer.js
@@ -5,7 +5,7 @@ function HikeContainer({ match, hikes, currentUser, addAlertMessage }) {
   let id = +match.params.hikeId.replace((/hike_/), "")
   let hike;
 
-  if (hikes.length > 0 ) {
+  if (hikes.length > 0 && hikes.find(hike => hike.id === id)) {
     hike = hikes.find(hike => hike.id === id)
   } else if ( currentUser.favHikes.length > 0 ){
     hike = currentUser.favHikes.find(favHike => favHike.id === id)
