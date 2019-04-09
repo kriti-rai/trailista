4d1381aa887aed15d0c043ed201769a5e386fc45
github username <github email address>
2019-04-08 22:37:03 -0700

    Render ErrorPage if hike not found

--- client/src/containers/Hikes/HikeContainer.js
+++ client/src/containers/Hikes/HikeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hike from '../.././components/Hikes/Hike';
+import ErrorPage from '../.././components/ErrorPage';
 
 function HikeContainer({ match, hikes, currentUser, addAlertMessage }) {
   let id = +match.params.hikeId.replace((/hike_/), "")
@@ -7,17 +8,13 @@ function HikeContainer({ match, hikes, currentUser, addAlertMessage }) {
 
   if (hikes.length > 0 && hikes.find(hike => hike.id === id)) {
     hike = hikes.find(hike => hike.id === id)
-  } else if ( currentUser.favHikes.length > 0 ){
+    return <Hike hike={ hike } />
+  } else if ( currentUser.favHikes.length > 0 && currentUser.favHikes.find(favHike => favHike.id === id)){
     hike = currentUser.favHikes.find(favHike => favHike.id === id)
-  } else (
-    addAlertMessage({
-      text: "Something went wrong or the requested hike was not found",
-      type: "error" })
-  )
-
-  return (
-    <Hike hike={ hike } />
-  )
+    return <Hike hike={ hike } />
+  } else {
+    return <ErrorPage />
+  }
 }
 
 export default HikeContainer;
