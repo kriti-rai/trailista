6e2863d364238146f5351fd43629f6efc0fc67ea
github username <github email address>
2019-04-10 00:03:10 -0700

    Add classes

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -25,13 +25,11 @@ function Navbar(props) {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+    <nav className="navbar">
           <NavLink to='/hikes' activeClassName="selected-tab" className="nav-link"><span className="tab-title">Explore</span></NavLink>
           <NavLink to='/about' activeClassName="selected-tab" className="nav-link"><span className="tab-title">About</span></NavLink>
           <NavLink to='/' className="nav-link"><span className="app-title">Trail</span><i id="hiker-icon" className="fas fa-hiking"></i><span className="app-title">sta</span></NavLink>
           { showTabs }
-        </div>
       </nav>
     </>
   )
