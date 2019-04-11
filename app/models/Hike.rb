5cb2847f07145653f12f8b750424a8e93324b66d
github username <github email address>
2019-04-10 17:27:43 -0700

    Remove comments association

--- app/models/Hike.rb
+++ app/models/Hike.rb
@@ -1,6 +1,4 @@
 class Hike < ApplicationRecord
-  #has title, overview, image, length, ascent, descent, ratings, difficulty, comments
-  has_many :comments
   has_many :fav_hikes
   has_many :users, through: :fav_hikes
 end
