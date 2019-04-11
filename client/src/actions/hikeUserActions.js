76908c2e639179b6bf34f4114840c5c4ece5b835
github username <github email address>
2019-04-11 13:19:21 -0700

    Add clearFavorites()

--- client/src/actions/hikeUserActions.js
+++ client/src/actions/hikeUserActions.js
@@ -8,9 +8,7 @@ let config = {
 
 export function favorite(token, hikeData) {
   config['headers']['Authorization'] = 'Bearer' + token
-
-  let data = { "hike" : hikeData }
-
+  let data = { "hike" : hikeData };
   return (dispatch) => {
     return axios.post(`${ API_URL }/add_favorite`, data, config)
       .then(response =>
@@ -27,14 +25,25 @@ export function favorite(token, hikeData) {
 
 export function deleteFavorite(token, id) {
   config['headers']['Authorization'] = 'Bearer' + token
-
-  let data = { "hike_id" : id }
-
+  let data = { "hike_id" : id };
   return (dispatch) => {
     return axios.post(`${ API_URL }/delete_favorite`, data, config)
       .then(response =>
         dispatch({
           type: 'DELETE_FAVORITE',
+          payload: response.data
+      })
+    )};
+}
+
+export function clearFavorites(token) {
+  config['headers']['Authorization'] = 'Bearer' + token
+  let data = {}
+  return (dispatch) => {
+    return axios.post(`${ API_URL }/clear_favorites`, data, config)
+      .then(response =>
+        dispatch({
+          type: 'CLEAR_FAVORITES',
           payload: response.data
       })
     )};
