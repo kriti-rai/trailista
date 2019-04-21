af45d68cd6813ced710d9080fac12acc08cc09aa
github username <github email address>
2019-04-20 17:59:42 -0700

    Convert to component and add componentDidMount()

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import SearchHikes from './Hikes/SearchHikes';
 
-function Home() {
-  return  (
-    <>
-    <div className="main-container">
-      <div className="home-container">
-        <div className="container">
-          <div className="d-flex justify-content-center h-100">
-            <p className="welcome-msg">Welcome to Trailista</p>
+class Home extends Component {
+
+  componentDidMount() {
+    let token = localStorage.getItem('jwt');
+    if (!!token) {
+       this.props.fetchCurrentUser(token);
+     }
+  }
+  render() {
+    return  (
+      <>
+      <div className="main-container">
+        <div className="home-container">
+          <div className="container">
+            <div className="d-flex justify-content-center h-100">
+              <p className="welcome-msg">Welcome to Trailista</p>
+            </div>
           </div>
+         </div>
         </div>
-       </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 };
 
 export default Home;
