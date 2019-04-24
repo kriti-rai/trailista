1a606e412f8837e12c56ac64446910039f646932
github username <github email address>
2019-04-23 20:40:18 -0700

    Wrap renderList in favorites-list class if > 0

--- client/src/components/Favorites/FavoritesList.js
+++ client/src/components/Favorites/FavoritesList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Favorite from './Favorite';
 
 function FavoritesList(props) {
@@ -6,15 +6,15 @@ function FavoritesList(props) {
   let renderList;
 
   if (props.favHikes.length > 0) {
-    renderList = props.favHikes.map((favHike, i) => <Favorite key={ i } deleteFavorite={ props.deleteFavorite } favHike={ favHike } />)
+    renderList = <div className="favorites-list">{ props.favHikes.map((favHike, i) => <Favorite key={ i } deleteFavorite={ props.deleteFavorite } favHike={ favHike } />) }</div>
   } else {
     renderList = <small>Currently, there is no item in here.</small>
   }
 
   return (
-    <div className="favorites-list">
+    <>
       { renderList }
-    </div>
+    </>
   )
 }
 
