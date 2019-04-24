4d139089970a127f670adbdd1ae0e13c51d04507
github username <github email address>
2019-04-23 21:26:16 -0700

    Import and use ClearAll component to represent clear all button

--- client/src/containers/FavoritesContainer.js
+++ client/src/containers/FavoritesContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import FavoritesList from '.././components/Favorites/FavoritesList';
+import ClearAll from '.././components/Favorites/ClearAll';
 
 const FavoritesContainer = (props) => {
     return (
       <div className="favorites-container">
         <h4>My Favorites &hearts;</h4>
-        <p className="btn btn-light btn-sm" onClick={() => { props.clearFavorites(props.token) } }><small>Clear All</small></p>
+        <ClearAll clearFavorites={ props.clearFavorites } token={ props.token}/>
         <br></br>
         <FavoritesList favHikes = { props.favHikes } deleteFavorite={ props.deleteFavorite } />
       </div>
