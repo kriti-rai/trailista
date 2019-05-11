c571a58868e647aef9c0102d9f6704b8c3d515eb
github username <github email address>
2019-05-11 01:12:03 -0700

    Define openMenu(); add hamburger menu button

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -22,13 +22,26 @@ function Navbar(props) {
     )
   }
 
+  let openMenu = (e) => {
+    e.preventDefault();
+    let x = document.getElementById("topNav");
+    if (x.className === "navbar") {
+      x.className = "topnav responsive";
+    } else {
+      x.className = "navbar"
+    }
+  }
+
   return (
     <>
-    <nav className="navbar">
-          <NavLink to='/hikes' activeClassName="selected-tab" className="nav-link"><span className="tab-title">Explore</span></NavLink>
-          <NavLink to='/about' activeClassName="selected-tab" className="nav-link"><span className="tab-title">About</span></NavLink>
-          <NavLink to='/' className="nav-link"><span className="app-title">Trail</span><i id="hiker-icon" className="fas fa-hiking"></i><span className="app-title">sta</span></NavLink>
-          { showTabs }
+      <nav className="navbar" id="topNav">
+        <NavLink to='/hikes' activeClassName="selected-tab" className="nav-link"><span className="tab-title">Explore</span></NavLink>
+        <NavLink to='/about' activeClassName="selected-tab" className="nav-link"><span className="tab-title">About</span></NavLink>
+        <NavLink to='/' className="nav-link"><span className="app-title">Trail</span><i id="hiker-icon" className="fas fa-hiking"></i><span className="app-title">sta</span></NavLink>
+        { showTabs }
+        <button onClick={ openMenu } id="navbarButton" className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+          aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i
+          className="fas fa-bars fa-1x"></i></span></button>
       </nav>
     </>
   )
