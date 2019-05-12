afccb5282888fca93db007ca461fbfe5d1b85e41
github username <github email address>
2019-05-11 23:21:29 -0700

    Format form

--- client/src/components/Hikes/SearchHikesForm.js
+++ client/src/components/Hikes/SearchHikesForm.js
@@ -5,22 +5,21 @@ const SearchHikesForm = (props) => {
   return (
     <div className="search-hike-form" >
       <form onSubmit={ props.getCoordinates }>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">City</span>
+        <div className="form-row">
+          <div className="city-input">
+            <input className='form-control' placeholder="City" type="text" name="city" value={ props.city } onChange={ props.handleChange }/>
           </div>
-          <input className='form-control width100' placeholder="Enter a city..." type="text" name="city" value={ props.city } onChange={ props.handleChange }/>
-          <div className="input-group-prepend">
-            <span className="input-group-text">Country</span>
+          <div className="country-input">
+            <CountryDropdown className='form-control' name="countryCode" value={ props.country } onChange={ (val) => props.handleChangeCountry(val)} />
           </div>
-          <CountryDropdown className='form-control width100' name="countryCode" value={ props.country } onChange={ (val) => props.handleChangeCountry(val)} />
-          <div className="input-group-prepend">
+          <div>
             <span className="input-group-text">Max Distance</span>
           </div>
-          <input className='form-control width100' placeholder="Enter max distance..." type="number" value={ props.maxDistance } name="maxDistance"  onChange={ props.handleChange }/>
-
-          <span className="input-group-btn"><button className="btn btn-info" type="submit"><i className="fa fa-search"></i></button></span>
+          <div className="max-distance-input">
+            <input className='form-control' type="number" value={ props.maxDistance } name="maxDistance"  onChange={ props.handleChange }/>
           </div>
+          <span className="input-group-btn"><button className="search-button" type="submit"><i className="fa fa-search"></i></button></span>
+        </div>
       </form>
     </div>
   )
