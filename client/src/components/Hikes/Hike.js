3394cf940acb85c0dfc584858dd7c4bb5900f0ba
github username <github email address>
2019-05-15 15:03:33 -0700

    Add hike-infocard-container class

--- client/src/components/Hikes/Hike.js
+++ client/src/components/Hikes/Hike.js
@@ -24,23 +24,26 @@ const Hike = ({ hike }) => {
       <h3 className="display-4 hike-page-header">{ hike.name }</h3>
       <p className="display-5 hike-page-sub-header">{ hike.location }</p>
       <div className="hike-card-container">
-        <div className="hike-info-card">
-          <div className="hike-info-title"><i id="hike-info-hike-icon" className="fas fa-hiking"></i>&nbsp; Trail</div>
-          <div className='hike-info'>Length: <strong>{ hike.length }</strong></div>
-          <div className='hike-info'>Difficulty: <strong>{ difficulty() }</strong></div>
-          <div className='hike-info'>Rating: <strong>{ hike.stars }</strong></div>
+        <div className="hike-infocard-container">
+          <div className="hike-info-card">
+            <div className="hike-info-title"><i id="hike-info-hike-icon" className="fas fa-hiking"></i>&nbsp; Trail</div>
+            <div className='hike-info'>Length: <strong>{ hike.length }</strong></div>
+            <div className='hike-info'>Difficulty: <strong>{ difficulty() }</strong></div>
+            <div className='hike-info'>Rating: <strong>{ hike.stars }</strong></div>
+          </div>
+          <div className="hike-info-card">
+            <div className="hike-info-title"><i id="hike-info-mountain-icon" className="fas fa-mountain"></i>&nbsp; Elevation</div>
+            <div className='hike-info'>Ascent: <strong>{ hike.ascent }'</strong></div>
+            <div className='hike-info'>Descent: <strong>{ hike.descent }'</strong></div>
+            <div className='hike-info'>High: <strong>{ hike.high }'</strong></div>
+            <div className='hike-info'>Low: <strong>{ hike.low }'</strong></div>
+          </div>
         </div>
-        <div className="hike-info-card">
-          <div className="hike-info-title"><i id="hike-info-mountain-icon" className="fas fa-mountain"></i>&nbsp; Elevation</div>
-          <div className='hike-info'>Ascent: <strong>{ hike.ascent }'</strong></div>
-          <div className='hike-info'>Descent: <strong>{ hike.descent }'</strong></div>
-          <div className='hike-info'>High: <strong>{ hike.high }'</strong></div>
-          <div className='hike-info'>Low: <strong>{ hike.low }'</strong></div>
-        </div>
+        <br></br>
+        <br></br>
+        <p><img className="hike-image" src={ hike.imgMedium} /></p>
+        <p className='summary'>Overview: { hike.summary }</p>
       </div>
-      <br></br>
-      <p><img className="hike-image" src={ hike.imgMedium} /></p>
-      <p className='summary'>Overview: { hike.summary }</p>
     </div>
   )
 }
