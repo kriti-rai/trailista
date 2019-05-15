09b96c40ce7eea1427555a3e016fcb79d56f2866
github username <github email address>
2019-05-15 15:28:57 -0700

    Create using postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_01_005457) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fav_hikes", force: :cascade do |t|
     t.integer "hike_id"
     t.integer "user_id"
