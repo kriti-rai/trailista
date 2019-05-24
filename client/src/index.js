2165b1a56d473e20dc52011cf4aab83496238fc8
github username <github email address>
2019-05-23 19:40:51 -0700

    Remove dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
